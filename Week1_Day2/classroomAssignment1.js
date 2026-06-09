c6b2f0a89edce3050572a8143d2478a7ead54e98
vidhya85 <57653234+vidhya85@users.noreply.github.com>
2026-06-09 17:34:00 +0530

    Add files via upload

---
+++ Week1_Day2/classroomAssignment1.js
@@ -0,0 +1,15 @@
+let firstName='Srividhya';
+let companyName='Testleaf';
+let mobileNumber=9940065939;
+let isAutomation=false;
+let hasPlaywright;
+console.log(firstName);
+console.log(typeof firstName);
+console.log(companyName);
+console.log(typeof companyName);
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+console.log(isAutomation);
+console.log(typeof isAutomation);
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
---
+++ Week1_Day2/classroomAssignment2.js
@@ -0,0 +1,17 @@
+const browserVersion='Chrome'
+
+function getBrowserVersion1(){
+    if(true){
+        var browserVersion='Edge'
+                   }
+    console.log(browserVersion);
+    }
+    getBrowserVersion1()
+
+    function getBrowserVersion2(){
+        if(true){
+            let browserVersion='Edge'
+        }
+        console.log(browserVersion);
+    }
+    getBrowserVersion2()
